d94d4bdeddcb06aa208ba6e70b83e5b38175d325
JackTorrence <jacktorrence420@gmx.com>
2018-08-27 16:24:42 +0100

    Add project files.

---
+++ DataProtectionApi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataProtectionApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
